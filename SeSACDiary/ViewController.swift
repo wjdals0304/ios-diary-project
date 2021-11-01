ac6ff69dc8f12817b851006ddb82da901099ee7e
jmkim <wjdals0304@gmail.com>
2021-11-01 15:26:05 +0900

    Initial Commit

---
+++ SeSACDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACDiary
+//
+//  Created by 김정민 on 2021/11/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
