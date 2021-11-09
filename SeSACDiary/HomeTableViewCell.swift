e7a23aeaa794de169a191ac46680bf1f223dd7cc
jmkim <wjdals0304@gmail.com>
2021-11-09 21:02:35 +0900

    awakeFromNib 이용한 리팩토링

--- SeSACDiary/HomeTableViewCell.swift
+++ SeSACDiary/HomeTableViewCell.swift
@@ -15,12 +15,22 @@ class HomeTableViewCell: UITableViewCell {
     @IBOutlet weak var collectionView: UICollectionView!
     
     
+    var data: [String] = [] {
+        didSet {
+            collectionView.reloadData()
+            categoryLabel.text = "\(data.count)개"
+        }
+    }
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         
-        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.isPagingEnabled = true
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -29,4 +39,52 @@ class HomeTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+}
+
+
+extension HomeTableViewCell: UICollectionViewDelegate,UICollectionViewDataSource , UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCollectionViewCell.identifier, for: indexPath) as? HomeCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.imageView.backgroundColor = .brown
+        //array[collectionView.tag] -> ["a","a","a","a"]
+        cell.contentLabel.text = data[indexPath.item]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        if collectionView.tag == 0 {
+            return CGSize(width: UIScreen.main.bounds.width, height: 100)
+            
+        } else {
+            return CGSize(width: 150, height: 100)
+
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        if collectionView.tag == 0 {
+            return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        } else {
+            return UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return collectionView.tag == 0 ? 0 : 10
+    }
+    
+    
 }
--- SeSACDiary/HomewViewController.swift
+++ SeSACDiary/HomewViewController.swift
@@ -12,14 +12,23 @@ class HomewViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
     
+    let array = [
+        Array(repeating: "a", count: 20),
+        Array(repeating: "b", count: 10),
+        Array(repeating: "c", count: 15),
+        Array(repeating: "d", count: 2),
+        Array(repeating: "e", count: 13),
+        Array(repeating: "f", count: 22),
+        Array(repeating: "g", count: 13),
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = LocalizableStrings.home.localized
         
         tableView.delegate = self
         tableView.dataSource = self
-
-
     }
     
     @IBAction func AddviewButton(_ sender: UIBarButtonItem) {
@@ -45,7 +54,7 @@ class HomewViewController: UIViewController {
 extension HomewViewController: UITableViewDelegate,UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return array.count
 
     }
     
@@ -54,17 +63,15 @@ extension HomewViewController: UITableViewDelegate,UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.identifier, for: indexPath) as? HomeTableViewCell else { return UITableViewCell() }
         
-        cell.collectionView.dataSource = self
-        cell.collectionView.delegate = self
+        cell.data = array[indexPath.row]
         cell.collectionView.tag = indexPath.row
-        cell.collectionView.isPagingEnabled = true
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
-        return indexPath.row == 1 ? 300 : 170
+        return indexPath.row == 1 ? 300 : 300
 
 
     }
@@ -72,46 +79,3 @@ extension HomewViewController: UITableViewDelegate,UITableViewDataSource {
 }
 
 
-extension HomewViewController: UICollectionViewDelegate,UICollectionViewDataSource , UICollectionViewDelegateFlowLayout{
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCollectionViewCell.identifier, for: indexPath) as? HomeCollectionViewCell else {
-            return UICollectionViewCell()
-        }
-        
-        cell.imageView.backgroundColor = .brown
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        if collectionView.tag == 0 {
-            return CGSize(width: UIScreen.main.bounds.width, height: 100)
-            
-        } else {
-            return CGSize(width: 150, height: 100)
-
-        }
-        
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        
-        if collectionView.tag == 0 {
-            return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        } else {
-            return UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
-        }
-        
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return collectionView.tag == 0 ? 0 : 10
-    }
-    
-    
-}
